8c176494bb35e22f2283fdbc23b8282dd83224e0
JUSST <88431113+nguyenthanhtung2k4@users.noreply.github.com>
2024-11-11 22:03:29 +0700

    fix main error

--- Main.py
+++ Main.py
@@ -5,16 +5,11 @@ from time import*
 import  csv
 import os, sys
 import re,json
-
 #  try expcet cai thu vien pystyle 
 try:
      from pystyle import System # type: ignore
 except :
      os.system('pip install pystyle')
-     
-'''
-16,8,4,15,12,13
-'''
 def TextMenu(choce,text_small):
 
      if choce==1:
@@ -316,74 +311,6 @@ def menu16(file):
           else:
                     print(f'\t\t   {RESETs+color_bar}Danh Sách Trống rỗng !{RESETs}')
                     quit;
-def about():
-     System.Clear()
-     about=f'''{RESETs+colorB}
-\t\tVề chúng tôi{RESETs+colorF}
-\tChương trình được phát triển bởi các sinh viên Đại Nam phát triển
- - Các sinh viên thực hiện:
-                    Nguyễn Thanh Tùng
-                    Lê Văn Vượng
-                    Bùi Quang Tuấn
-Chương trình được thiết kế để giúp các khách sạn nâng cao chất lượng dịch vụ,tối ưu hóa quy trình hoạt động và tăng doanh thu.Chúng tôi phát triển gồm 20 chức năng để quản lý khách sản một cách tận tiện nhất
-{RESETs}
-'''
-     for  i  in about:
-          sys.stdout.write(i)
-          sleep(0.025)
-def Update_Color():
-     f={
-     "F1":"\033[30m"  ,     # black
-     "F2":"\033[31m",       # red
-     "F3":"\033[32m"  ,     # green
-     "F4":"\033[33m"   ,    # yellow
-     "F5":"\033[34m" ,      # blue
-     "F6":"\033[35m"    ,   # magenta
-     "F7":"\033[36m" ,      # cyan
-     "F8":"\033[37m"  ,     # white
-     "F0":"\033[39m"      # reset
-     }
-     # /////////// Back color
-     b={
-          "B1":"\033[40m"  ,    # black
-          "B2":"\033[41m",      # red
-          "B3":"\033[42m"  ,    # green
-          "B4":"\033[43m"   ,   # yellow
-          "B5":"\033[44m" ,     # blue
-          "B6":"\033[45m"    ,  # magenta
-          "B7":"\033[46m" ,     # cyan
-          "B8":"\033[47m"  ,    # white
-          "B0":"\033[49m"      # reset
-     }
-     with open(file_Setting,'r')as file:
-          data=json.load(file)
-          Font_Logo=data['Setting']['Font_Logo']
-          Back_Logo=data['Setting']['Back_Logo']
-          Bar_Logo=data['Setting']['Bar_Logo']
-          Font_color=data['Setting']['Font_Color']
-          Back_color=data['Setting']['Back_Color']
-          Font_ERROR=data['Setting']['Font_ERROR']
-          Back_ERROR=data['Setting']['Back_ERROR']
-          for i  in f :# check font
-               if  Font_color== i:
-                    Font_color=f[i]
-               if  Font_ERROR== i:
-                    Font_ERROR=f[i]
-               if Bar_Logo == i:
-                    Bar_Logo=f[i]
-               if Font_Logo== i:
-                    Font_Logo=f[i]
-          for  j in b:#check back
-               if  Back_color== j:
-                    Back_color=b[j]
-               if Back_ERROR == j:
-                    Back_ERROR =b[j]
-               if Back_Logo==j:
-                    Back_Logo=b[j]
-     Back_RESET="\033[49m";Font_RESET="\033[39m"
-     RESETS=Back_RESET+Font_RESET         
-     return Font_Logo,Back_Logo,Bar_Logo ,Font_color,Back_color,Font_ERROR,Back_ERROR,RESETS;
-
 def read_Table_color():
      with open(file_Setting,'r') as f:
           data=json.load(f)
@@ -395,7 +322,7 @@ def setting(file_setting):
      nhap=INput(int,'Nhập ',2)
      if nhap==1:
           System.Clear()
-          version='TVT_1.10'
+          version='TVT_2.00'
           print(f'\n\nVersion: {version}\n\n')
      elif nhap==2:
           print(f'''
@@ -469,10 +396,7 @@ if  __name__=='__main__':
           18: lambda: setting(file_Setting),
           19: lambda: about(),
      }
-# //////  update color
-     # Update_Color()
-     colorF_logo,colorB_logo,color_bar, colorF,colorB,erF,erB,RESETs=Update_Color()
-     # print(colorF,colorB,erF,erB)
+     
 # //////////////////// TEST
      # setting(file_Setting)
      
