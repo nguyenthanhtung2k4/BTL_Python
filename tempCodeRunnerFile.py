7bad412560adfa07d8332a72512cc5c1637ea611
JUSST <88431113+nguyenthanhtung2k4@users.noreply.github.com>
2024-11-11 12:12:05 +0700

    hoan hien  các menu  cua team

--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-# from datetime import datetime
-
-# day1='2024-10-12'
-# day2='2024-10-30'
-# day1 = datetime.strptime(day1, '%Y-%m-%d')
-# day2 = datetime.strptime(day2, '%Y-%m-%d')
-# day = (day2 - day1).days
-# print(day)
